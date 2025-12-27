59ef0e21117340b0177b901f484b9ed21f391d1c
Paulo Santos <pauloxrms@gmail.com>
2025-12-27 17:48:06 -0300

    feat: updates fetch and creates import script
    Signed-off-by: Paulo Santos <pauloxrms@gmail.com>

---
+++ import_dotfiles.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+cp ./.zshrc ~/.zshrc
+
+cp -r ./hypr/* ~/.config/hypr/
+cp -r ./waybar/* ~/.config/waybar/
+cp -r ./rofi/* ~/.config/rofi/
+cp -r ./nvim/* ~/.config/nvim/
+cp -r ./kitty/* ~/.config/kitty/
+cp -r ./fastfetch/* ~/.config/fastfetch/
