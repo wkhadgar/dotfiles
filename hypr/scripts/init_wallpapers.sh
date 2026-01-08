4d16e8500b600fb557c73ceca964ebcf2f1a8e81
Paulo Santos <pauloxrms@gmail.com>
2026-01-08 20:23:06 -0300

    fix: upgrade config files for hypland v0.5
    Signed-off-by: Paulo Santos <pauloxrms@gmail.com>

--- hypr/scripts/init_wallpapers.sh
+++ hypr/scripts/init_wallpapers.sh
@@ -2,13 +2,13 @@
 
 WALLPAPER_DIR="$HOME/wallpapers"
 
-WALLPAPER_A="$WALLPAPER_DIR/326951.jpg"
-WALLPAPER_B="$WALLPAPER_DIR/713774.jpg"
+WALLPAPER_A="$WALLPAPER_DIR/side-a.jpg"
+WALLPAPER_B="$WALLPAPER_DIR/side-b.jpg"
 
 # Initialize hyprpaper
 hyprpaper &
 sleep 2
 
 # Apply the selected wallpaper
-hyprctl hyprpaper reload HDMI-A-1,contain:"$WALLPAPER_A"
-hyprctl hyprpaper reload DP-1,contain:"$WALLPAPER_B"
+hyprctl hyprpaper wallpaper DP-1, "$WALLPAPER_A"
+hyprctl hyprpaper wallpaper HDMI-A-1, "$WALLPAPER_B"
